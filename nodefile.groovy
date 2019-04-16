70311763a5fb675c375e9875881a9bb7cb5c5878
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-16 11:48:09 +0530

    Update nodefile.groovy

--- nodefile.groovy
+++ nodefile.groovy
@@ -5,6 +5,7 @@ node {
            if (i==0)
            {
                git "https://github.com/SujataKale97/gitnew.git"
+               credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c'
                echo 'Running on Stage #0'
            }
            else {
